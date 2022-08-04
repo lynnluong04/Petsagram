f9e9bf0cfe9227da96d23df9ff097234d5aaeda9
Lynn Luong <lynn.h.luong@gmail.com>
2022-08-04 18:34:50 -0400

    single post modal works

--- react-app/src/components/SinglePostModal.js
+++ react-app/src/components/SinglePostModal.js
@@ -1,22 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Modal } from "../context/Modal";
 import SinglePost from "./SinglePost";
-
+import { useHistory } from "react-router-dom";
 
 const SinglePostModal = () => {
     const [ showModal, setShowModal ] = useState(false);
     const { postId } = useParams();
+    const history = useHistory();
+    const { userId } = useParams();
+
+    useEffect(()=> {
+        setShowModal(true)
+    }, [])
 
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <button onClick={() => setShowModal(true)}></button>
-            </div>
+            </div> */}
             {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
+                <Modal onClose={() => {
+                    setShowModal(false)
+                    history.push(`/${userId}`)
+                    }}>
                    <SinglePost postId={Number(postId)}/>
                 </Modal>
             )}
