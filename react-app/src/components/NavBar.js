15b23c0d2b3270c5068023b34b41cf11683902a2
Lynn Luong <lynn.h.luong@gmail.com>
2022-08-09 11:12:43 -0400

    working on navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import "./css/navbar.css"
 
 const NavBar = () => {
   return (
@@ -9,7 +10,7 @@ const NavBar = () => {
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            <img className='logo' src="https://www.linkpicture.com/q/Screen-Shot-2022-08-08-at-8.39.47-PM_1.png"/>
           </NavLink>
         </li>
         <li>
