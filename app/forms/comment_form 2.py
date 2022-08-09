32bfda53c99cbb4b97affc1b3673bf6d378b490c
Lynn Luong <lynn.h.luong@gmail.com>
2022-08-09 05:21:53 -0400

    deleted duplicate files

--- app/forms/comment_form 2.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateTimeField
-from wtforms.validators import DataRequired
-
-class CommentForm(FlaskForm):
-    post_id = IntegerField("Post Id", [DataRequired()])
-    owner_id = IntegerField("Owner Id", [DataRequired()])
-    content = StringField("Content", [DataRequired()])
-    created_at = DateTimeField('created_at', format="%Y-%m-%d %H:%M:%S", validators=[DataRequired()])
--- app/forms/edit_comment 2.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class EditComment(FlaskForm):
-    content = StringField("Content", [DataRequired()])
