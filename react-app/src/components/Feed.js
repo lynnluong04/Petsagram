3e8e86c92fe08e5bc81b01f9b9aade5afa451d57
Lynn Luong <lynn.h.luong@gmail.com>
2022-08-10 19:02:07 -0400

    preview works, trying crop

--- react-app/src/components/Feed.js
+++ react-app/src/components/Feed.js
@@ -37,7 +37,7 @@ const Feed = () => {
                                 </NavLink>
                             </div>
                             <div className='post-menu'>
-                                <svg aria-label="More options" class="_ab6-" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
+                                <svg aria-label="More options"  color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
                             </div>
                         </div>
                         <img className='post' src={post.media_url} alt="photo post" />
