e317e231f23c29c3caa4bec5c7422cd313169885
Lynn Luong <lynn.h.luong@gmail.com>
2022-08-12 14:07:50 -0400

    added signup length validations and no whitespace in pass

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo, Length
 from app.models import User
 
 
@@ -20,35 +20,38 @@ def username_exists(form, field):
         raise ValidationError(
             'This username isn\'t available. Please try another.')
 
+def no_whitespace(form, field):
+    password = field.data
+    if password.__contains__(" "):
+        raise ValidationError ("Password cannot contain whitespace")
 
 class SignUpForm(FlaskForm):
     username = StringField('username',
                            validators=[
                                DataRequired(message="You must enter a username"),
                                username_exists,
-
+                               Length(min=3, max=40, message="Username must be between 3 to 40 characters")
                            ])
+
     name = StringField('name',
                        validators=[
                            DataRequired(message="You must enter your full name"),
-
-
+                           Length(min=1, max=100, message="Name must be between 1 to 100 characters")
                        ])
     email = StringField('email',
                         validators=[
                             DataRequired(message="You must enter an email"),
                             user_exists,
                             Email(),
-
-
+                            Length(min=6, max=255, message="Email must be between 6 to 255 characters")
                         ])
     password = StringField('password',
                            validators=[
                                DataRequired(message="You must enter a password"),
                                EqualTo('confirm',
                                        message='Passwords must match'),
-
-
+                                Length(min=6, max=255, message="Password must be between 6 to 255 characters"),
+                                no_whitespace
                            ])
     confirm = StringField('confirm',
                                    validators=[
