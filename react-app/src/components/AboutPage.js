109cd61a5dd49ce9ca3eac85975c594d6bc3ff2d
Lynn Luong <lynn.h.luong@gmail.com>
2022-10-11 13:24:52 -0400

    styling about

--- react-app/src/components/AboutPage.js
+++ react-app/src/components/AboutPage.js
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
+import "./css/about.css"
+const IMAGE = (imgName) => {
+    return require(`./images/${imgName}`).default
+}
 
+const AboutPage = ({ loadAbout }) => {
 
-const AboutPage = ({loadAbout}) => {
-
-    useEffect (()=> {
+    useEffect(() => {
         loadAbout();
     })
 
     return (
         <div>
+            <div className='about-container'>
+                <div className='about-top'>Lynn Luong</div>
+                <img className='about-pic' src={IMAGE("about.png")} alt="picture of developer" />
+                <div>
+                    <i class="fa-brands fa-linkedin"></i>
+                    <i class="fa-brands fa-github"></i>
+                </div>
+            </div>
             About page coming soon!
         </div>
     )
