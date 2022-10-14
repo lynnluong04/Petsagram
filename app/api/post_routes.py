f50364248965615f62a85a76063f691abb3a890e
Lynn Luong <lynn.h.luong@gmail.com>
2022-10-13 20:05:46 -0400

    sorting posts in backend

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from flask_login import current_user, login_required
 from app.forms.edit_post import EditPost
 from app.forms.post_form import PostForm
+from sqlalchemy import or_
 from app.models import Post, db
 from app.s3_helpers import (
     upload_file_to_s3, allowed_file, get_unique_filename)
@@ -11,15 +12,18 @@ post_routes = Blueprint('posts', __name__)
 @post_routes.route('/')
 @login_required
 def followed_posts():
-    following_posts = Post.query.join(
-    follows, (follows.c.followee == Post.owner_id)).filter(
-        follows.c.follower == current_user.id).all()
-    my_posts = Post.query.filter_by(owner_id=current_user.id).all()
+    posts = Post.query.join(
+        follows, (follows.c.followee == Post.owner_id)).filter(or_(
+        follows.c.follower == current_user.id,
+        Post.owner_id == current_user.id)).order_by(Post.id.desc()).all()
 
-    posts = my_posts + following_posts
-    # print("FROM THE BACKEND ROUTE----------------------------------------")
-    # print([post.to_dict() for post in posts])
-    # print("-----------------------------------------------------------")
+    # following_posts = Post.query.join(
+    # follows, (follows.c.followee == Post.owner_id)).filter(
+    #     follows.c.follower == current_user.id).all()
+    # my_posts = Post.query.filter_by(owner_id=current_user.id).all()
+
+    # posts = my_posts + following_posts
+
     return {'posts': [post.to_dict() for post in posts]}
 
 @post_routes.route('/<int:userId>')
