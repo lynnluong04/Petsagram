37b7cb003a7dfd93e73ec3d91b5353fa7291bde9
Lynn Luong <lynn.h.luong@gmail.com>
2022-10-14 01:25:55 -0400

    test 2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -28,7 +28,7 @@ function App() {
   useEffect(() => {
     (async () => {
       await dispatch(authenticate());
-      await dispatch(thunkLoadUsers());
+      // await dispatch(thunkLoadUsers());
       setLoaded(true);
     })();
   }, [dispatch]);
