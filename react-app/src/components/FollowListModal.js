04b0845fbd095466d815bd65eb59bf2dc9dd01a2
Lynn Luong <lynn.h.luong@gmail.com>
2022-10-18 13:12:57 -0400

    bold

--- react-app/src/components/FollowListModal.js
+++ react-app/src/components/FollowListModal.js
@@ -10,7 +10,7 @@ const FollowListModal = ({ usersList, isFollowers }) => {
     return (
         <div>
             {isFollowers ?
-                (usersList?.length === 1 ? <button className="counts" onClick={()=> setShowModal(true)} ><span>{usersList?.length}</span>  follower</button> :
+                (usersList?.length === 1 ? <button className="counts" onClick={()=> setShowModal(true)} ><span className='num' >{usersList?.length}</span>  follower</button> :
                     <button className="counts" onClick={()=> setShowModal(true)} ><span className='num'>{usersList?.length}</span> followers</button>
                 ) :
                 <button className="counts" onClick={()=> setShowModal(true)} ><span className='num'>{usersList.length}</span> following</button>
