a48dcd3900238c6c628a6b6d1d169d7c94a09b00
Lynn Luong <lynn.h.luong@gmail.com>
2023-02-16 19:53:52 -0500

    render deploy

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -11,7 +11,7 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
-    if environment == 'production':
+    # if environment == 'production':
         # # Before seeding, truncate all tables prefixed with schema name
         # db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
         # db.session.execute(f"TRUNCATE table {SCHEMA}.posts RESTART IDENTITY CASCADE;")
@@ -20,10 +20,10 @@ def seed():
         # db.session.commit()
         # undo_comments()
         # undo_posts()
-        undo_users()
+        # undo_users()
 
     seed_users()
-    # seed_posts()
+    seed_posts()
     # seed_comments()
     # Add other seed functions here
 
@@ -32,6 +32,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    # undo_posts()
+    undo_posts()
     # undo_comments()
     # Add other undo functions here
