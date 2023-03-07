cc10f1837a57b110ff09dd14dfd3758ea4023705
Lynn Luong <lynn.h.luong@gmail.com>
2023-03-06 23:23:50 -0500

    working on dm

--- react-app/src/components/Dm/DirectMessage.js
+++ react-app/src/components/Dm/DirectMessage.js
@@ -11,15 +11,79 @@ const DirectMessaging = () => {
     const [messages, setMessages] = useState([]);
     const [chatInput, setChatInput] = useState('');
     const [recipient, setRecipient] = useState([]);
+    const [validationErrors, setValidationErrors] = useState([]);
+    const [currentRecipient, setCurrentRecipient] = useState('')
     let recipients = []
+    let joinedId;
+    let roomId;
     const sessionUser = useSelector(state => state.session.user);
 
-
     useEffect(() => {
-        recipients.push(recipient.username)
+        if (recipient) recipients.push(recipient.username)
     }, [recipient])
 
-    console.log("!!!REC", recipients)
+    useEffect(() => {
+        const errors = [];
+        if (chatInput.length === 0) errors.push("Message body cannot be empty.");
+
+        setValidationErrors(errors);
+    }, [chatInput]);
+
+    useEffect(() => {
+        if (recipient) {
+            setCurrentRecipient(recipient)
+            joinedId = [sessionUser.id, recipient.id].sort();
+            roomId = `${joinedId[0]}-${joinedId[1]}`;
+            socket = io();
+
+            if (socket && recipient.id && sessionUser) socket.emit("dm_join", { username: sessionUser.username, dm_room_id: roomId })
+
+            //listen for chat events
+            socket.on('dm_chat', chat => {
+                // when receive a chat, add to messages state var
+                setMessages(messages => [...messages, chat]);
+                // console.log('chat in socket.on(dm_chat):', chat)
+            })
+
+            //when component unmounts, disconnect
+            return (() => {
+                // socket.removeAllListeners()
+                socket.emit('dm_leave', { username: sessionUser.username, recipient: recipient.id });
+                socket.disconnect();
+                setMessages([]);
+            })
+        }
+    }, [recipient, messages])
+
+    const sendChat = async (e) => {
+        e.preventDefault();
+
+        if (validationErrors.length === 0) {
+            //emit a message
+            if (recipient.id && sessionUser) socket.emit('dm_chat', { username: sessionUser.username, msg: chatInput, dm_room_id: roomId });
+
+            const dateTime = new Date();
+            const isoTime = dateTime.toISOString();
+            const date = isoTime.slice(0, 10);
+            const time = isoTime.slice(12, 19);
+            const combined = date + ' ' + time
+
+            // const payload = {
+            //     sender_id: sessionUser.id,
+            //     recipient_id: recipient.id,
+            //     message_body: chatInput,
+            //     created_at: combined
+            // }
+
+            // await dispatch(sendDmMessage(payload));
+            setChatInput('');
+        }
+
+
+        //clear input field after message is sent
+        setChatInput('');
+    }
+
     return (
         <div className='dm-container'>
             <div className='dm-container-left'>
@@ -33,7 +97,37 @@ const DirectMessaging = () => {
             </div>
 
             <div className='dm-container-right'>
+                {/* <div className='dm-history-container'>
+                    {sessionUser && (
+                        dmHistory && dmHistory?.map((message, idx) => (
+                            <div className='single-dm' key={idx}>
+                                <div className='dm-name'>
 
+                                    {message.sender_id === sessionUser.id ?
+                                        sessionUser?.username :
+                                        recipient?.username}
+                                </div>
+                                <div className='msg-body' >
+                                    {message.message_body ?
+                                        message?.message_body :
+                                        message?.msg}
+                                </div>
+                            </div>
+                        ))
+
+                    )}
+                </div> */}
+
+                <div className='chat-box-container'>
+                    <form onSubmit={sendChat} id='chat-box-form'>
+                        <input
+                            placeholder={`Message ${recipient?.username}`}
+                            value={chatInput}
+                        // onChange={updateChatInput}
+                        />
+                        {/* <button>Send</button> */}
+                    </form>
+                </div>
             </div>
 
         </div>
